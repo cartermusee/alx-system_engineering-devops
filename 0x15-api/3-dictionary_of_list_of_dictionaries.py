5f6f66d87c6b1e2b937c9c962523ed00d45fb6b3
cartermusee <cartermusee@gmail.com>
2023-10-23 03:42:30 -0700

    appi json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """json all users"""
-import requests
 import json
+import requests
 from sys import argv
 
 
@@ -17,11 +17,11 @@ if __name__ == "__main__":
         emp_id = user.get("id")
         emp_name = user.get("username")
         url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-        todo = url + "/todos"
+        todo = url + "/todos/"
         todo_res = requests.get(todo)
         todo_js = todo_res.json()
 
-        dictinary = {emp_id: []}
+        dictinary[emp_id] = []
         for task in todo_js:
             dictinary[emp_id].append({"username": emp_name,
                                       "task": task.get("title"),
