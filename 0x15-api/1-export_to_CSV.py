facd3c7eb0dd445b0741c49abbd57f5a20c04c81
cartermusee <cartermusee@gmail.com>
2023-10-23 02:44:43 -0700

    appi

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module for api saving to csv"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    emp_id = argv[1]
+
+    url = base_url + "/" + emp_id
+    api_req = requests.get(url)
+    emp_name = api_req.json().get("name")
+
+    todo = url + "/todos"
+    todo_res = requests.get(todo)
+    todo_js = todo_res.json()
+
+    with open("{}.csv".format(emp_id), "w") as file:
+        for task in todo_js:
+            file.write('"{}","{}","{}","{}"\n'.
+                       format(emp_id, emp_name, task.get('completed'),
+                              task.get('title')))
