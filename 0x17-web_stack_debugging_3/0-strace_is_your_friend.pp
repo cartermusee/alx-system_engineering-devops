c3a9afca3069cb54a3d4aaeca97805b84b0aa999
cartermusee <cartermusee@gmail.com>
2023-11-09 00:57:29 -0800

    apapche returnd 500

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fix returning a 500 error
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+debbugging 3
